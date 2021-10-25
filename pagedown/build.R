168d115724583bd252488660862dacc8e6cc8ec0
Kevin Cazelles <kevcaz@protonmail.com>
2021-10-25 11:12:30 -0400

    edit make for multiple lang

--- pagedown/build.R
+++ pagedown/build.R
@@ -1,8 +1,16 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly = TRUE)
+lang <- args[1]
+cat("LANG --->", lang, "\n")
+
 source("helpers.R")
 
-template <- readLines("template_cv_en.Rmd")
+tpl <- switch(lang, 
+  en = "template_main_en.Rmd",
+  stop("not available")
+  )
+template <- readLines(tpl)
 # NB by default whisker forward the parent envi and I used this
-
 
 writeLines(
   whisker::whisker.render(template, list(title = "KevCaz's CV")), 
